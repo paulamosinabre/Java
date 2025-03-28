5aac6df90ddfbbd65907e20d0392a824aa4e6188
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-03-28 09:06:03 +0800

    Student.java

---
+++ JAVA-Lessons/THIRD SEM/Student/Student.java
@@ -0,0 +1,14 @@
+package com.mycompany.main;
+
+public class Student {
+    
+    String name;
+    int age;
+    String program;
+    String yearLevel;
+    String course;
+    
+    void displayStudentInfo(){
+        System.out.println("Name: " + name +"\nAge: " + age + "\nProgram: " + program + "\nYear Level: " + yearLevel + "\nCourse: " + course);
+    }
+}
