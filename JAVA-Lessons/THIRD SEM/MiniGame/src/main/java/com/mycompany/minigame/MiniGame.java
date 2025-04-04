6dc62f2f6ad9ac7df48aaf4f304095bdb8f49498
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-04-04 09:43:29 +0800

    MiniGame.java

--- JAVA-Lessons/THIRD SEM/MiniGame/src/main/java/com/mycompany/minigame/MiniGame.java
+++ JAVA-Lessons/THIRD SEM/MiniGame/src/main/java/com/mycompany/minigame/MiniGame.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.minigame;
 
 import java.util.Scanner;
@@ -6,33 +5,42 @@ import java.util.Scanner;
 public class MiniGame {
 
     public static void main(String[] args) {
-        
+        enterInfo();
+    }
+
+    public static void enterInfo() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter character: ");
         String name = scan.nextLine();
-        
+
         System.out.print("Enter class type (Warrior/Mage/Archer): ");
         String classType = scan.nextLine();
-        
-        System.out.print("Enter initial level: ");
-        int level = scan.nextInt();
-        scan.nextLine();
-        
-        Character myCharacter = new Character(name, classType, level);
-        
-        System.out.println("\nCharacter Created!");
-        myCharacter.displayInfo();
-        
-        System.out.print("\nDo you want to level up? (yes/no): ");
-        String choose = scan.nextLine();
-        
-        if(choose.equalsIgnoreCase("yes")){
-            myCharacter.levelUp();
-            System.out.println("\nUpdated Character Info: " + "\nName: " + myCharacter.getName() + "\nClass: " + myCharacter.getClassType() + "\nLevel: " + myCharacter.getLevel());
-        } else if (choose.equalsIgnoreCase("no")){
-            System.out.println("Your level remains the same :/");
+
+        if (classType.equalsIgnoreCase("Warrior") || classType.equalsIgnoreCase("Mage") || classType.equalsIgnoreCase("Archer")) {
+            System.out.print("Enter initial level: ");
+            int level = scan.nextInt();
+            scan.nextLine();
+
+            Character myCharacter = new Character(name, classType, level);
+
+            System.out.println("\nCharacter Created!");
+            myCharacter.displayInfo();
+
+            System.out.print("\nDo you want to level up? (yes/no): ");
+            String choose = scan.nextLine();
+
+            if (choose.equalsIgnoreCase("yes")) {
+                myCharacter.levelUp();
+                System.out.println("\nUpdated Character Info: " + "\nName: " + myCharacter.getName() + "\nClass: " + myCharacter.getClassType() + "\nLevel: " + myCharacter.getLevel());
+            } else if (choose.equalsIgnoreCase("no")) {
+                System.out.println("Your level remains the same :/");
+            } else {
+                System.out.println("Invalid!");
+            }
         } else {
-            System.out.println("Invalid!");
-        }       
+            System.out.println("Invalid class type! Try Again");
+            enterInfo();
+        }
+
     }
 }
