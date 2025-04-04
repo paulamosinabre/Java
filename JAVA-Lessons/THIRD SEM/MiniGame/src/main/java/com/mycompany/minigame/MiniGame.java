c5ee551813d51d022214d9839152cf4247f4d7a3
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-04-04 08:47:52 +0800

    Add files via upload

---
+++ JAVA-Lessons/THIRD SEM/MiniGame/src/main/java/com/mycompany/minigame/MiniGame.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.minigame;
+
+import java.util.Scanner;
+
+public class MiniGame {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter character: ");
+        String name = scan.nextLine();
+        
+        System.out.print("Enter class type (Warrior/Mage/Archer): ");
+        String classType = scan.nextLine();
+        
+        System.out.print("Enter initial level: ");
+        int level = scan.nextInt();
+        scan.nextLine();
+        
+        Character myCharacter = new Character(name, classType, level);
+        
+        System.out.println("\nCharacter Created!");
+        myCharacter.displayInfo();
+        
+        System.out.print("\nDo you want to level up? (yes/no): ");
+        String choose = scan.nextLine();
+        
+        if(choose.equalsIgnoreCase("yes")){
+            myCharacter.levelUp();
+            System.out.println("\nUpdated Character Info: " + "\nName: " + myCharacter.getName() + "\nClass: " + myCharacter.getClassType() + "\nLevel: " + myCharacter.getLevel());
+        } else if (choose.equalsIgnoreCase("no")){
+            System.out.println("Your level remains the same :/");
+        } else {
+            System.out.println("Invalid!");
+        }       
+    }
+}
