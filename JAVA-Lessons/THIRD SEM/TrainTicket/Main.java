02cc0a389e649c45a8600cbed0bc7afa2020d6ff
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-04-10 21:23:26 +0800

    Main.java

---
+++ JAVA-Lessons/THIRD SEM/TrainTicket/Main.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        TrainTicket ticket = new TrainTicket();
+        ticket.setFare(25.50);
+        System.out.println("Train Fare: " + ticket.getFare());
+    }
+}
