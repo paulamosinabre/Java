7b38c1a8a8d48ea0a05ea449ed892e1191099787
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-04-11 21:05:28 +0800

    Calculator.java

--- JAVA-Lessons/THIRD SEM/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
+++ JAVA-Lessons/THIRD SEM/Calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -13,54 +13,69 @@ public class Calculator {
         String password = scan.nextLine();
 
         calculators.setPassword(password);
-        enterInformation();
+        numberChooser();
     }
 
-    public static void enterInformation() {
+    public static void numberChooser() {
 
-        try {
-            System.out.print("Enter first number: ");
-            double num1 = scan.nextInt();
+        while (true) {
+            try {
+                System.out.print("\nEnter first number: ");
+                num1 = scan.nextInt();
 
-            System.out.print("Enter second number: ");
-            double num2 = scan.nextInt();
-        } catch (Exception e) {
-            System.out.println("Invalid number! Please Try Again.");
-            enterInformation();
+                System.out.print("Enter second number: ");
+                num2 = scan.nextInt();
+
+                operationChooser();
+            } catch (Exception e) {
+                System.out.println("Invalid number! Please Try Again.");
+                scan.nextLine();
+            }
         }
+    }
 
+    public static void operationChooser() {
         try {
             System.out.print("\nChoose an operation: ");
-            System.out.println("\n1 - Add" + "\n2 - Subtract" + "\n3 - Multiply" + "\n4 - Divide" + "\n5 - Power(Secret Operation)");
+            System.out.println("\n1 - Add" + "\n2 - Subtract" + "\n3 - Multiply" + "\n4 - Divide" + "\n5 - Power(Secret Operation)" + "\n6 - Stop");
             System.out.print("Enter your choice: ");
             int choose = scan.nextInt();
             scan.nextLine();
             switch (choose) {
                 case 1:
                     calculators.add(num1, num2);
+                    numberChooser();
                     break;
                 case 2:
                     calculators.subtract(num1, num2);
+                    numberChooser();
                     break;
 
                 case 3:
                     calculators.multiply(num1, num2);
+                    numberChooser();
                     break;
 
                 case 4:
                     calculators.divide(num1, num2);
+                    numberChooser();
                     break;
                 case 5:
                     enterPassword();
+
                     break;
 
+                case 6:
+                    System.exit(0);
+                    break;
                 default:
                     System.out.println("Invalid number! Please Try Again.");
-                    enterInformation();
+                    operationChooser();
             }
         } catch (Exception e) {
-            System.out.println("Invalid number! Please Try Again.");
-            enterInformation();
+            System.out.println("Invalid! Please Try Again.");
+            scan.nextLine();
+            numberChooser();
         }
 
     }
@@ -73,8 +88,7 @@ public class Calculator {
             calculators.power(num1, num2);
         } else {
             System.out.println("Access denied. We are proceeding with basic operations only.");
-            enterInformation();
+            numberChooser();
         }
     }
-
 }
