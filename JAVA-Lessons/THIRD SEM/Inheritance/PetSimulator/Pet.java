027d9d77d695d07ac2f66ed54e88b7986889548a
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-04-19 19:43:34 +0800

    Pet.java

---
+++ JAVA-Lessons/THIRD SEM/Inheritance/PetSimulator/Pet.java
@@ -0,0 +1,31 @@
+package com.mycompany.petsimulator;
+
+public class Pet {
+    
+    private String name;
+    private int age;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    
+    public int getAge(){
+        return this.age;
+    }
+    
+    public void  speak(){
+        System.out.println("");
+    }
+    
+    public void getInfo(){
+        System.out.println("");
+    }
+}
