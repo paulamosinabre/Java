94fa2f91e98dbf515ae7db6c1fe464d91e80d9a6
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-05-13 19:15:37 +0800

    Update Login.java

--- JAVA-PROJECT-FINALS/FRESHIE/THIRD-SEM/Console/Login.java
+++ JAVA-PROJECT-FINALS/FRESHIE/THIRD-SEM/Console/Login.java
@@ -4,6 +4,7 @@ package com.mycompany.taskmanagement;
 public class Login {
     
     private String email, firstName, lastName, username, password;
+    private TaskManager taskManager;
     
     public Login(String email, String firstName, String lastName, String username, String password){
         this.email = email;
@@ -11,6 +12,7 @@ public class Login {
         this.lastName = lastName;
         this.username = username;
         this.password = password;
+        this.taskManager = new TaskManager();
     }
     
     public String getUsername(){
@@ -21,9 +23,11 @@ public class Login {
         return this.password;
     }
     
+    public TaskManager getTaskManager(){
+        return this.taskManager;
+    }
     public void showInformation(){
         System.out.println("E-mail: " + email + "\nFirst Name: " + firstName + "\nLast Name: " + lastName + "\nUsername: " + username);
     }
 
 }
-
