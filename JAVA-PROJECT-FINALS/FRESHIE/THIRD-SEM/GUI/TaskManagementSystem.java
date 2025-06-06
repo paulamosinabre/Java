8c414173cb4837978cd83c949f6bc570bfbd523d
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-06-06 15:03:47 +0800

    Add files via upload

---
+++ JAVA-PROJECT-FINALS/FRESHIE/THIRD-SEM/GUI/TaskManagementSystem.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.taskmanagementsystem;
+
+public class TaskManagementSystem {
+
+    public static void main(String[] args) {
+        
+        FrontPage fPage = new FrontPage();
+        fPage.setVisible(true);
+        fPage.setLocationRelativeTo(null);
+        
+        
+        /*
+        Home home = new Home();
+        home.setVisible(true);
+        home.setLocationRelativeTo(null);
+        */
+        
+    }
+}
