4ce6ad33ba23bde8063994a725bc6a371fd3f4ed
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-06-12 22:18:39 +0800

    Update Task.java

--- JAVA-PROJECT-FINALS/FRESHIE/THIRD-SEM/GUI/Task.java
+++ JAVA-PROJECT-FINALS/FRESHIE/THIRD-SEM/GUI/Task.java
@@ -1,44 +1,43 @@
 
 package com.mycompany.taskmanagementsystem;
 
-import java.util.ArrayList;
-
 public class Task {
 
-    private String task, category, priority, date;
+    private String task, category, status, priority, date;
 
-    public Task(String task, String category, String priority, String date) {
+    public Task(String task, String category, String status, String priority, String date) {
         this.task = task;
         this.category = category;
         this.priority = priority;
         this.date = date;
     }
 
-    public String getTask() {
-        return task;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public String getPriority() {
-        return priority;
-    }
-
-    public String getDate() {
-        return date;
-    }
+    public String getTask() { return task; }
     
-    public void setTask(String task){
-        this.task = task;
+    public String getCategory() { return category; }
+    
+    public String getStatus() { return status; }
+    
+    public String getPriority() { return priority; }
+    
+    public String getDate() { return date; }
+    
+    public String getTypeOrSubject(){
+        return "";
+    }
+
+    public void setTask(String task) { this.task = task; }
+
+    public void showDetails() {
+        System.out.println("Task: " + task
+                + "\nCategory: " + category
+                + "\nPriority: " + priority
+                + "\nDate: " + date); 
     }
     @Override
     public String toString() {
-        return "Task: " + task
-                + "\nCategory: " + category
-                + "\nPriority: " + priority
-                + "\nDate: " + date;
+        return "               " + task + "          " + category + "         " + priority + "      " + date;
     }
 }
+
 
