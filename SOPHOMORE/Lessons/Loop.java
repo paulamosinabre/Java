a4c46272e6fb9aa6269aa433250a87b5c04ff5cc
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-07-31 19:08:30 +0800

    Create Loop.java

---
+++ SOPHOMORE/Lessons/Loop.java
@@ -0,0 +1,46 @@
+
+class Main {
+    public static void main(String[] args) {
+        
+        int i = 13;
+        do{
+            
+            System.out.println(i);
+            i += 3;
+        } while (i <=29);
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        
+        int i = 100;
+        do{
+           System.out.println(i);
+            i /= 3;
+        } while (i >=25);
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        
+        int i = 1;
+         
+        while (i < 25){
+            System.out.println(i);
+            i *= 2;
+        }
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+            for(int i = 5; i >= -10; i-=3){
+            System.out.println(i);
+        }
+    }
+}
