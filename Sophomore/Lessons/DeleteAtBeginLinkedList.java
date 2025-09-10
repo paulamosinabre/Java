c4d4aa21f3a0539e4cdcaa46d40a64759826704b
Paula Bianca Mosinabre <mosinabre.paulabianca@gmail.com>
2025-09-10 20:58:04 +0800

    Create DeleteAtBeginLinkedList.java

---
+++ Sophomore/Lessons/DeleteAtBeginLinkedList.java
@@ -0,0 +1,62 @@
+
+class Main {
+    
+    static class node {
+        int data; //stores value
+        node next; //reference to the next node
+    
+    
+        node(int value){
+            data = value;
+            next = null; 
+        }
+    }
+    
+    //head pointer, points to the first node in the linked list
+    static node head;
+    
+    static void printList(){
+        node p = head;
+        System.out.print("[");
+        
+        while(p != null){
+            System.out.print(" " + p.data + " ");
+            p = p.next;
+        }
+        
+        System.out.print("]");
+    }
+    
+    static void insertatbegin(int data) {
+        node linked = new node(data);
+        
+        //point new node next to current head
+        linked.next = head;
+        
+        //move head to point to the new node
+        head = linked;
+    }
+    
+    static void deleteatbegin(){
+        head = head.next;
+    }
+    
+    public static void main(String[] args) {
+        insertatbegin(12);
+        insertatbegin(22);
+        insertatbegin(30);
+        insertatbegin(44);
+        insertatbegin(50);
+        insertatbegin(33);
+        
+        System.out.println("Linked List: ");
+        printList();
+        
+        //the output is reversed because you are inserting at the beginning
+        deleteatbegin();
+        System.out.println("\nLinked List after deletion: ");
+        printList();
+        
+        
+    }
+}
